5db731ec28e27b7f8cd71e63ac65e8bc600cfae4
Greg Pequeno <58681191+grepexdev@users.noreply.github.com>
2024-05-08 19:54:40 +0000

    chore: Update PeopleService to fetch and return an array of people

--- leadership/src/app/app.component.ts
+++ leadership/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PeopleService } from '../shared/people.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'leadership';
+  peopleArray: any;
+
+  constructor(private peopleService: PeopleService) { }
+
+  getPeople() {
+    this.peopleService.getPeople().subscribe(people => {
+      this.peopleArray = people
+    })
+  }
+
+  ngOnInit() {
+    this.getPeople();
+  }
 }
