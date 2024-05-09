80b11391c4d3a149a489abe0e0b31d4bde0b05f4
Greg Pequeno <58681191+grepexdev@users.noreply.github.com>
2024-05-09 02:20:20 +0000

    feat: Add lazy loading for bonds and stocks components

---
+++ lazyloading-example/src/app/bonds/bonds.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonds',
+  templateUrl: './bonds.component.html',
+  styleUrl: './bonds.component.css'
+})
+export class BondsComponent {
+
+}
