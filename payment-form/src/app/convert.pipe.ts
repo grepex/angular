b50a3de468a73ef9d45b0714feaaed9867acee3d
Grep <grepexdev@gmail.com>
2024-05-09 23:07:37 -0500

    updates

---
+++ payment-form/src/app/convert.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert',
+  standalone: true
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
---
+++ template-driven-form-example/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, NgIf, NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  title = 'Template Driven Form';
+  @ViewChild('f', { static: false })
+  signupForm!: NgForm;
+  defaultQuestion = 'street';
+  answer = '';
+  honorifics = ['Dr', 'Mr.', 'Mrs.', 'Ms.'];
+  user = {
+    username: '',
+    email: '',
+    securityQuestion: '',
+    answer: '',
+    honorific: ''
+  };
+  submitted = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.securityQuestion = this.signupForm.value.security;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.honorific = this.signupForm.value.honorific;
+    this.signupForm.reset();
+  }
+}
